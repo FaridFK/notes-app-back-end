cd4a78f47915509ebdf9fd4e64ad7eb3b1a7da65
FaridFK <74176005+FaridFK@users.noreply.github.com>
2023-03-20 19:17:21 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,42 @@
-const routes = [{
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes',
-        handler: getAllNotesHandler,
-    },
-    {
-        method: 'GET',
-        path: '/notes/{id}',
-        handler: getNoteByIdHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/notes/{id}',
-        handler: editNoteByIdHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/notes/{id}',
-        handler: deleteNoteByIdHandler,
-    },
-];
-
 const {
-    addNoteHandler,
-    getAllNotesHandler,
-    getNoteByIdHandler,
-    editNoteByIdHandler,
-    deleteNoteByIdHandler,
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
 } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => 'Halaman tidak ditemukan',
+  },
+];
 
 module.exports = routes;
